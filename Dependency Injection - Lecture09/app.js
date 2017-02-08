3cdc2b79caffd9284318c41bae29402a690dd1a9
cansel muti <aaa@aaa.com>
2017-02-08 19:26:59 +0300

    Dependencies

---
+++ Dependency Injection - Lecture09/app.js
@@ -0,0 +1,29 @@
+(function () {
+'use strict';
+
+angular.module('DIApp', [])
+.controller('DIController', ['$scope', '$filter', DIController]);
+
+function DIController ($scope,
+                        $filter,
+                      $injector) {
+  $scope.name = "Muti";
+
+  $scope.upper = function () {
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name)
+  };
+
+  console.log($injector.annotate(DIController));//fonksiyonun argümanlarının bir dizisini döndürür
+}
+
+function AnnotateMe(name, job, blah) {
+  return "Blah";
+}
+
+console.log(AnnotateMe()); // Fonksiyonu çalıştırıp dönen değeri ekrana basar(console a)
+console.log(AnnotateMe);
+console.log(AnnotateMe.toString()); // Üstteki satırla ile aynı şeyi yapar
+console.log(DIController.toString());
+
+})();
